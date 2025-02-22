0b75966f58f4557fe2ef4dc675233f04af2f4a41
Gustavo Vasquez <gustavovasquez2002@gmail.com>
2025-02-22 15:55:20 -0300

    feat: add auth with jwt

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,9 +1,18 @@
+import "dotenv/config";
+import { verify } from "jsonwebtoken";
+
 export const authenticate = (req, res, next) => {
-  const authHeader = req.headers.authorization;
+	const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ error: 'Unauthorized' });
-  }
+	const [authType, token] = authHeader.split(" ");
 
-  next();
+	if (!authHeader || !authType !== "Bearer") {
+		return res.status(401).json({ error: "Unauthorized" });
+	}
+
+	verify(token, process.env.JWT_SECRET, (err, _) => {
+		if (err) return res.status(403).json({ error: "Invalid Token" });
+	});
+
+	next();
 };
