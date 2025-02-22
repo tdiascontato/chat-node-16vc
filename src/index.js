48efcff0890973fc47aa03b2577425905d3b9ce5
Tiago Dias <tdiascontato@gmail.com>
2025-02-22 15:56:30 -0300

    feat: cors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import bodyParser from 'body-parser';
+import cors from 'cors'; 
 import prisma from './prisma.js';
 import userRouter from './routes/userRoute.js';
 import conversationRouter from './routes/conversationRoute.js';
@@ -9,7 +10,21 @@ import messageRouter from './routes/messageRoute.js';
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://127.0.0.1:5500'], // Altere para seus domínios
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+const io = new Server(server, {
+  cors: {
+    origin: ['http://localhost:3000', 'http://127.0.0.1:5500'], // Mesmos domínios do Express
+    methods: ['GET', 'POST'],
+    credentials: true
+  }
+});
+// origin: '*'
 
 app.use(bodyParser.json());
 app.set('io', io);
@@ -36,6 +51,12 @@ io.on('connection', (socket) => {
       console.error('Erro na conexão:', error);
     }
   });
+
+  socket.on('leaveConversation', (conversationId) => {
+    socket.leave(conversationId.toString());
+    console.log(`Cliente saiu da conversa ${conversationId}`);
+  });
+
 
   socket.on('disconnect', () => {
     console.log('Cliente desconectado');
