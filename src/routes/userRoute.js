6724b02e843e7ebbbb9327ccebfcfc85b6a016da
Gustavo Vasquez <gustavovasquez2002@gmail.com>
2025-02-22 17:24:11 -0300

    feat: add users get list

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,31 +1,46 @@
-import { Router } from 'express';
-import prisma from '../prisma.js';
+import { Router } from "express";
+import prisma from "../prisma.js";
 
 const router = Router();
 
-router.get('/:id', async (req, res) => {
-  try {
-    const user = await prisma.user.findUnique({
-      where: { uid: req.params.id },
-      select: {
-        uid: true,
-        name: true,
-        email: true,
-        profilePicture: true
-      }
-    });
+router.get("/", async (req, res) => {
+	try {
+		const users = await prisma.user.findMany({
+			select: {
+				uid: true,
+				name: true,
+			},
+		});
 
-    if (!user) return res.status(404).json({ error: 'Usuário não encontrado' });
-    
-    res.json({
-      user_id: user.uid,
-      name: user.name,
-      email: user.email,
-      profile_picture: user.profilePicture
-    });
-  } catch (error) {
-    res.status(500).json({ error: 'Erro ao buscar usuário' });
-  }
+		res.json(users);
+	} catch (error) {
+		res.status(500).json({ error: "Erro ao buscar usuário" });
+	}
+});
+
+router.get("/:id", async (req, res) => {
+	try {
+		const user = await prisma.user.findUnique({
+			where: { uid: req.params.id },
+			select: {
+				uid: true,
+				name: true,
+				email: true,
+				profilePicture: true,
+			},
+		});
+
+		if (!user) return res.status(404).json({ error: "Usuário não encontrado" });
+
+		res.json({
+			user_id: user.uid,
+			name: user.name,
+			email: user.email,
+			profile_picture: user.profilePicture,
+		});
+	} catch (error) {
+		res.status(500).json({ error: "Erro ao buscar usuário" });
+	}
 });
 
 export default router;
